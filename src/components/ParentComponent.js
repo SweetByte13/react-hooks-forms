8a723c3188fe767177ac4cd0336e6a9019ce8ffe
Tova Yocheved Hillman <hillman.t.y@gmail.com>
2024-04-24 15:03:01 -0400

    done

---
+++ src/components/ParentComponent.js
@@ -0,0 +1,34 @@
+import react, { useState } from 'react';
+import Form from './Form';
+import DisplayData from './DisplayData';
+
+function ParentComponent() {
+    const [firstName, setFirstName] = useState("John");
+    const [lastName, setLastName] = useState("Henry");
+
+    function handleFirstNameChange(e) {
+        setFirstName(e.target.value)
+        console.log(e.target.value)
+    }
+
+    function handleLastNameChange(e) {
+        setLastName(e.target.value)
+    }
+    return (
+        <div><Form
+            firstName={firstName}
+            lastName={lastName}
+            handleFirstNameChange={handleFirstNameChange}
+            handleLastNameChange={handleLastNameChange}
+        />
+            <DisplayData
+                firstName={firstName}
+                lastName={lastName}
+                handleFirstNameChange={handleFirstNameChange}
+                handleLastNameChange={handleLastNameChange}
+                />
+        </div>
+    );
+}
+
+export default ParentComponent;
